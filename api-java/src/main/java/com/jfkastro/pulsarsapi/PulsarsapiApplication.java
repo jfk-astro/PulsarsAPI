d48eb94faf0bfbd3ed7f7cc10b6e7fa9f21dea31
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-06-13 22:38:48 -0700

    [Add] Java Initialization
    - Add Springboot project.
    - Add Spring Web, Spring JPA, and Spring Docker compose support.

---
+++ api-java/src/main/java/com/jfkastro/pulsarsapi/PulsarsapiApplication.java
@@ -0,0 +1,13 @@
+package com.jfkastro.pulsarsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsarsapiApplication.class, args);
+	}
+
+}
---
+++ api-java/src/test/java/com/jfkastro/pulsarsapi/PulsarsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfkastro.pulsarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
