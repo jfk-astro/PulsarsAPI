fc39af96be82a4147e14e7326d044840340a9ef6
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-06-13 23:31:03 -0700

    [Add] Python Initialization & Code
    - Add Python code to load database data into a CSV.
    - Add a requirements.txt file with all the requirements.

---
+++ fetch/fetch_data.py
@@ -0,0 +1,27 @@
+import psrqpy
+import pandas as pd
+import os
+
+def fetch_pulsar_data():
+    query = psrqpy.QueryATNF(params=[
+        "PSRJ",         # Name
+        "P0",           # Period
+        "P1",           # Period Derivative
+        "AGE",          # Age
+        "BSURF",        # Surface Magnetic Field
+        "DIST",         # Distance
+        "DM",           # Dispersion Measure
+        "EDOT"          # Spin-down Luminosity
+    ])
+
+    df = query.table
+
+    return df
+
+def save_to_csv(df, filename="../data/pulsars_data.csv"):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    df.to_csv(filename, index=False)
+
+if __name__ == "__main__":
+    df = fetch_pulsar_data()
+    save_to_csv(df)
